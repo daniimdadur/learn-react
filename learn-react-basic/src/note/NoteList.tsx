68cede3d66ee4cf17a5b98268f748136d80ec81e
Dani Imdadur <dir131203@gmail.com>
2026-09-07 16:24:19 +0700

    feat: memo

--- learn-react-basic/src/note/NoteList.tsx
+++ learn-react-basic/src/note/NoteList.tsx
@@ -1,19 +1,39 @@
 import Note, {type NoteObject} from "./Note.tsx";
 import * as React from "react";
 import {NotesContext} from "./NoteContext.tsx";
+import {type RefObject, useMemo, useRef, useState} from "react";
 
 export default function NoteList() : React.ReactElement {
     const notes: NoteObject[] | null = React.useContext(NotesContext);
-    if (!notes) {
+    const [search, setSearch] = useState<string>("");
+    const searchInput: RefObject<HTMLInputElement | null> = useRef<HTMLInputElement>(null);
+
+    const filteredNotes: NoteObject[] | undefined = useMemo((): NoteObject[] | undefined => {
+        console.info("filtering notes");
+        return notes?.filter(note => note.text.includes(search))
+    }, [notes, search]);
+
+    function handleSearch(): void {
+        if (searchInput.current) {
+            console.info("handleSearch");
+            setSearch(searchInput.current.value);
+        }
+    }
+
+    if (!filteredNotes) {
         return <p>No notes available.</p>;
     }
     return (
-        <ul>
-            {notes.map((note: NoteObject): React.ReactElement => (
-                <li key={note.id}>
-                    <Note note={note}/>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <input ref={searchInput} placeholder="Search"/>
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+                {filteredNotes.map((note: NoteObject): React.ReactElement => (
+                    <li key={note.id}>
+                        <Note note={note}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
