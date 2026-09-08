0a7287fb6498ec308b86237d296a1597a2c801ae
Dani Imdadur <dir131203@gmail.com>
2026-09-08 14:57:27 +0700

    feat: initation react router project

---
+++ learn-react-router/src/About.tsx
@@ -0,0 +1,12 @@
+import type {ReactElement} from "react";
+
+function About(): ReactElement {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Welcome to the about page!</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ learn-react-router/src/Home.tsx
@@ -0,0 +1,12 @@
+import type {ReactElement} from "react";
+
+function Home(): ReactElement {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to the home page!</p>
+        </div>
+    )
+}
+
+export default Home;
