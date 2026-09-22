9b83da0bab57492b4370df0cabbfe4a8b7a37acd
Dani Imdadur <dir131203@gmail.com>
2026-09-22 14:29:09 +0700

    feat: add dashboard

--- learn-react-contact-management/src/main.tsx
+++ learn-react-contact-management/src/main.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "./components/Layout.tsx";
 import UserRegister from "./components/user/UserRegister.tsx";
 import UserLogin from "./components/user/UserLogin.tsx";
+import DashboardLayout from "./components/DashboardLayout.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,8 +14,8 @@ createRoot(document.getElementById('root')!).render(
                   <Route path="/register" element={<UserRegister/>}/>
                   <Route path="/login" element={<UserLogin/>}/>
               </Route>
-              <Route path="/dashboard">
-
+              <Route path="/dashboard" element={<DashboardLayout/>}>
+                  <Route path="contacts" element={<div>contacts</div>}/>
               </Route>
           </Routes>
       </BrowserRouter>
